d999d373dce4b061c812068b756f09de97a795ed
Saasha <saashm@uw.edu>
2020-04-07 21:41:21 +0400

    something basic working

--- app/src/main/java/com/saashm/dotify/MainActivity.kt
+++ app/src/main/java/com/saashm/dotify/MainActivity.kt
@@ -2,11 +2,16 @@ package com.saashm.dotify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-
+    private val num = Random.nextInt(1000, 99999)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val playContent = "$num plays"
+        tvNumPlays.text = playContent
     }
 }
