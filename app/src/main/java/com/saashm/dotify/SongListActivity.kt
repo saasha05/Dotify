a011bc807b764ed94b9fe4ffbbf250b1bd28efac
Saasha <saashm@uw.edu>
2020-04-21 12:46:32 +0400

    im boreddd icon update

--- app/src/main/java/com/saashm/dotify/SongListActivity.kt
+++ app/src/main/java/com/saashm/dotify/SongListActivity.kt
@@ -16,6 +16,7 @@ class SongListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_song_list)
+        title = "All Songs"
         var allSongs: List<Song> = SongDataProvider.getAllSongs()
         val songAdapter = SongAdapter(allSongs)
 
